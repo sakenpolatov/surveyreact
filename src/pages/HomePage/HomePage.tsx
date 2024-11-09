7e0488af903f0294e871dc0570de1f44449e9fcd
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-11-09 12:56:12 +0500

    feat: add button to route

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,17 @@
-export const HomePage = () => {
-	return <div>HomePage</div>
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export const HomePage: React.FC = () => {
+	const navigate = useNavigate()
+
+	const handleNavigate = () => {
+		navigate('/survey')
+	}
+
+	return (
+		<div>
+			<h1>HomePage</h1>
+			<button onClick={handleNavigate}>перейти к опросу</button>
+		</div>
+	)
 }
--- src/pages/SurveyPage/SurveyPage.tsx
+++ src/pages/SurveyPage/SurveyPage.tsx
@@ -1,3 +1,7 @@
-export const SurveyPage = () => {
-	return <div>SurveyPage</div>
+export const SurveyPage: React.FC = () => {
+	return (
+		<div>
+			<h1>SurveyPage</h1>
+		</div>
+	)
 }
