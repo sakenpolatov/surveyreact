dbf5480272e7a3cee4c08d3bdeae376381702d78
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-11-11 13:15:06 +0500

    feat: add redux

---
+++ src/redux/slices/surveySlice.ts
@@ -0,0 +1,51 @@
+// surveySlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface Result {
+	question: string
+	userAnswer: string
+	correctAnswer: string
+	isCorrect: boolean
+}
+
+interface SurveyState {
+	results: Result[]
+	correctCount: number
+	isCompleted: boolean
+	showSurvey: boolean
+}
+
+const initialState: SurveyState = {
+	results: [],
+	correctCount: 0,
+	isCompleted: false,
+	showSurvey: true
+}
+
+const surveySlice = createSlice({
+	name: 'survey',
+	initialState,
+	reducers: {
+		setResults: (
+			state,
+			action: PayloadAction<{ results: Result[]; correctCount: number }>
+		) => {
+			state.results = action.payload.results
+			state.correctCount = action.payload.correctCount
+			state.isCompleted = true
+			state.showSurvey = false
+		},
+		resetSurvey: state => {
+			state.results = []
+			state.correctCount = 0
+			state.isCompleted = false
+			state.showSurvey = true
+		},
+		toggleShowSurvey: state => {
+			state.showSurvey = !state.showSurvey
+		}
+	}
+})
+
+export const { setResults, resetSurvey, toggleShowSurvey } = surveySlice.actions
+export default surveySlice.reducer
