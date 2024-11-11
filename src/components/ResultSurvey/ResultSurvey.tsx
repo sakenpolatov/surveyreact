e14c653deada822ddc1d31fa3120cbc836fc7d1f
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-11-11 13:28:20 +0500

    style: fix scroll

--- src/components/ResultSurvey/ResultSurvey.tsx
+++ src/components/ResultSurvey/ResultSurvey.tsx
@@ -10,41 +10,43 @@ export const ResultSurvey: React.FC = () => {
 
 	return (
 		<div className={styles.container}>
-			<h2 className={styles.header}>Результаты:</h2>
-			<p
-				className={`${styles.correctCount} ${
-					isPerfectScore ? '' : styles.incorrect
-				}`}
-			>
-				Правильных ответов: {correctCount}/{results.length}
-			</p>
-			<button
-				className={styles.retryButton}
-				onClick={() => {
-					dispatch(resetSurvey())
-				}}
-			>
-				Retry
-			</button>
-			<ul className={styles.resultsList}>
-				{results.map((result, index) => (
-					<li key={index} className={styles.resultItem}>
-						<div className={styles.question}>
-							{index + 1}. {result.question}
-						</div>
-						<div className={styles.answer}>
-							<span>Ваш ответ: </span>
-							<span className={styles.userAnswer}>{result.userAnswer}</span>
-						</div>
-						<div className={styles.answer}>
-							<span>Правильный ответ: </span>
-							<span className={styles.correctAnswer}>
-								{result.correctAnswer}
-							</span>
-						</div>
-					</li>
-				))}
-			</ul>
+			<div className={styles.content}>
+				<h2 className={styles.header}>Результаты:</h2>
+				<p
+					className={`${styles.correctCount} ${
+						isPerfectScore ? '' : styles.incorrect
+					}`}
+				>
+					Правильных ответов: {correctCount}/{results.length}
+				</p>
+				<button
+					className={styles.retryButton}
+					onClick={() => {
+						dispatch(resetSurvey())
+					}}
+				>
+					Retry
+				</button>
+				<ul className={styles.resultsList}>
+					{results.map((result, index) => (
+						<li key={index} className={styles.resultItem}>
+							<div className={styles.question}>
+								{index + 1}. {result.question}
+							</div>
+							<div className={styles.answer}>
+								<span>Ваш ответ: </span>
+								<span className={styles.userAnswer}>{result.userAnswer}</span>
+							</div>
+							<div className={styles.answer}>
+								<span>Правильный ответ: </span>
+								<span className={styles.correctAnswer}>
+									{result.correctAnswer}
+								</span>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	)
 }
