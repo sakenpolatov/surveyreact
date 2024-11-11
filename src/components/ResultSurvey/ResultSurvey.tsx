32063e3009c97f1ba759683c3687847175aeb3d7
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-11-11 13:16:05 +0500

    feat: add results info

---
+++ src/components/ResultSurvey/ResultSurvey.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useAppSelector, useAppDispatch } from '../../redux/hooks'
+import { resetSurvey } from '../../redux/slices/surveySlice'
+import styles from './result.module.scss'
+
+export const ResultSurvey: React.FC = () => {
+	const { results, correctCount } = useAppSelector(state => state.survey)
+	const dispatch = useAppDispatch()
+	const isPerfectScore = correctCount === results.length
+
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.header}>Результаты:</h2>
+			<p
+				className={`${styles.correctCount} ${
+					isPerfectScore ? '' : styles.incorrect
+				}`}
+			>
+				Правильных ответов: {correctCount}/{results.length}
+			</p>
+			<button
+				className={styles.retryButton}
+				onClick={() => {
+					dispatch(resetSurvey())
+				}}
+			>
+				Retry
+			</button>
+			<ul className={styles.resultsList}>
+				{results.map((result, index) => (
+					<li key={index} className={styles.resultItem}>
+						<div className={styles.question}>
+							{index + 1}. {result.question}
+						</div>
+						<div className={styles.answer}>
+							<span>Ваш ответ: </span>
+							<span className={styles.userAnswer}>{result.userAnswer}</span>
+						</div>
+						<div className={styles.answer}>
+							<span>Правильный ответ: </span>
+							<span className={styles.correctAnswer}>
+								{result.correctAnswer}
+							</span>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
